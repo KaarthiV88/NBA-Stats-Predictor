a73f36edd72435fa2d3d31ec2ff253a250860f94
KaarthiV88 <39105708+KaarthiV88@users.noreply.github.com>
2025-05-15 22:28:32 -0500

    Update PP_AI.py to test functions from bet_calculations.py

--- PP_AI.py
+++ PP_AI.py
@@ -1,10 +1,15 @@
 import nba_api
-import pandas
+import pandas as pd
 import tabulate
+import bet_calculations as bc
+from nba_api.stats.endpoints import playergamelog as pgl
 
-nba_player = str(input("Name of NBA Player: "))
+
+playerName = str(input("Name of NBA Player: "))
 bet_category = int(input("""What category is the bet:\n1) Points\n2) Rebounds\n3) Assists\n4) Points+Rebounds+Assists 
 5) Points+Rebounds\n6) Points+Assists\n7) Assists+Rebounds\n"""))
+
+opposingTeam = str(input("Enter the abbreviation of the opposing team: "))
 
 dict_category = {
     1: "Points",
@@ -16,4 +21,6 @@ dict_category = {
     7: "Assists+Rebounds"
 }
 
-print("Your bet is on the", nba_player, dict_category[bet_category])
+print("Your bet is on the", playerName, dict_category[bet_category], "\n\n")
+
+pgl.PlayerGameLog(player_id=bc.get_player_id(playerName), season='2024-25', season_type_all_star='Regular Season')
